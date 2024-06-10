ed845d4d4ee2d2fdd10fabfe94b05db13f52a13a
msschwartz21 <msschwartz21@gmail.com>
2024-06-10 16:44:24 -0700

    Initial commit from tlambert03/pyrepo-cookiecutter

---
+++ src/dlmbl_unet/__init__.py
@@ -0,0 +1,10 @@
+"""A basic UNet backbone for DL@MBL exercises"""
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("dlmbl-unet")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+__author__ = "Morgan Schwartz"
+__email__ = "msschwartz21@gmail.com"
