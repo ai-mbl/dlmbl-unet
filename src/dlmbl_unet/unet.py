88e69186bca7ce6556d8c9b9ab6de9104550d2b5
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2024-06-28 17:43:58 -0400

    docs: :memo: improve docstrings and errors for invalid literals

--- src/dlmbl_unet/unet.py
+++ src/dlmbl_unet/unet.py
@@ -11,23 +11,31 @@ class ConvBlock(torch.nn.Module):
         in_channels: int,
         out_channels: int,
         kernel_size: int,
-        padding: str = "same",
+        padding: Literal["same", "SAME", "valid", "VALID"] = "same",
         ndim: Literal[2, 3] = 2,
     ):
-        """A convolution block for a U-Net. Contains two convolutions, each followed by a ReLU.
-
+        """
         Args:
-        ----
-            in_channels (int): The number of input channels for this conv block. Depends on
-                the layer and side of the U-Net and the hyperparameters.
-            out_channels (int): The number of output channels for this conv block. Depends on
-                the layer and side of the U-Net and the hyperparameters.
-            kernel_size (int): The size of the kernel. A kernel size of N signifies an
-                NxN square kernel.
-            padding (str, optional): The type of padding to use. Options are "same" or "valid".
-                Defaults to "same".
+            in_channels (int): The number of input channels for this conv block.
+            out_channels (int): The number of output channels for this conv block.
+            kernel_size (int): The size of the kernel. A kernel size of N signifies an NxN or NxNxN
+                kernel for ``ndim=2`` and ``ndim=3``, respectively.
+            padding (Literal["same", "SAME", "valid", "VALID"], optional): The type of padding to
+                use. "same" or "SAME" means padding is added to preserve the input dimensions.
+                "valid" or "VALID" means no padding is added. Defaults to "same".
+            ndim (Literal[2, 3], optional): Number of dimensions for the convolution operation. Use
+                2 for 2D convolutions and 3 for 3D convolutions. Defaults to 2.
+
+        Raises:
+            ValueError: If unsupoorted values are used for padding or ndim.
         """
         super().__init__()
+        if padding not in ("valid", "VALID", "same", "SAME"):
+            msg = f"Invalid string value for padding: {padding=}. Options are same or valid."
+            raise ValueError(msg)
+        if ndim not in (2, 3):
+            msg = f"Invalid number of dimensions: {ndim=}. Options are 2 or 3."
+            raise ValueError(msg)
         convops = {2: torch.nn.Conv2d, 3: torch.nn.Conv3d}
         # determine padding size based on method
         if padding in ("VALID", "valid"):
@@ -56,8 +64,23 @@ class ConvBlock(torch.nn.Module):
 
 
 class Downsample(torch.nn.Module):
+    """Downsample module for U-Net"""
+
     def __init__(self, downsample_factor: int, ndim: Literal[2, 3] = 2):
+        """
+        Args:
+            downsample_factor (int): Factor by which to downsample featuer maps.
+            ndim (Literal[2,3], optional): Number of dimensions for the downsample operation.
+                Defaults to 2.
+
+        Raises:
+            ValueError: If unsupoorted value is used for ndim.
+        """
+
         super().__init__()
+        if ndim not in (2, 3):
+            msg = f"Invalid number of dimensions: {ndim=}. Options are 2 or 3."
+            raise ValueError(msg)
         downops = {2: torch.nn.MaxPool2d, 3: torch.nn.MaxPool3d}
         self.downsample_factor = downsample_factor
 
@@ -71,6 +94,17 @@ class Downsample(torch.nn.Module):
         return True
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Raises:
+            RuntimeError: If shape of input is not divisible by downsampling factor.
+
+        Returns:
+            torch.Tensor: Downsampled tensor.
+        """
         if not self.check_valid(tuple(x.size()[-2:])):
             raise RuntimeError(
                 f"Can not downsample shape {x.size()} with factor {self.downsample_factor}"
@@ -111,11 +145,18 @@ class OutputConv(torch.nn.Module):
         Args:
             in_channels (int): Number of input channels
             out_channels (int): Number of output channels
-            activation (torch.nn.Module  |  None, optional): An instance of any
-                torch activation function (e.g., ``torch.nn.ReLU()``). Defaults
-                to None for no activation after the convolution.
+            activation (torch.nn.Module  |  None, optional): An instance of any torch activation
+                function (e.g., ``torch.nn.ReLU()``). Defaults to None for no activation after the
+                convolution.
+            ndim (Literal[2,3], optional): Number of dimensions for the convolution operation.
+                Defaults to 2.
+        Raises:
+            ValueError: If unsupoorted values is used for ndim.
         """
         super().__init__()
+        if ndim not in (2, 3):
+            msg = f"Invalid number of dimensions: {ndim=}. Options are 2 or 3."
+            raise ValueError(msg)
         convops = {2: torch.nn.Conv2d, 3: torch.nn.Conv3d}
         self.final_conv = convops[ndim](
             in_channels, out_channels, 1, padding=0
@@ -140,12 +181,13 @@ class UNet(torch.nn.Module):
         fmap_inc_factor: int = 2,
         downsample_factor: int = 2,
         kernel_size: int = 3,
-        padding: str = "same",
+        padding: Literal["same", "SAME", "valid", "VALID"] = "same",
         upsample_mode: str = "nearest",
         ndim: Literal[2, 3] = 2,
     ):
-        """A U-Net for 2D input that expects tensors shaped like:
-        ``(batch, channels, height, width)``.
+        """A U-Net for 2D or 3D input that expects tensors shaped like:
+        ``(batch, channels, height, width)`` or ``(batch, channels, depth, height, width)``,
+        respectively.
 
         Args:
             depth (int): The number of levels in the U-Net. 2 is the smallest that really makes
@@ -163,14 +205,25 @@ class UNet(torch.nn.Module):
                 between levels. Defaults to 2.
             kernel_size (int, optional): Kernel size to use in convolutions on both sides of the
                 UNet. Defaults to 3.
-            padding (str, optional): Padding mode for convolutions.Either "same" or "valid".
-                Defaults to "same".
+            padding (Literal["same", "SAME", "valid", "VALID"], optional): The type of padding to
+                use. "same" or "SAME" means padding is added to preserve the input dimensions.
+                "valid" or "VALID" means no padding is added. Defaults to "same".
             upsample_mode (str, optional): The upsampling mode to pass to ``torch.nn.Upsample``.
                 Usually "nearest" or "bilinear". Defaults to "nearest".
+            ndim (Literal[2, 3], optional): Number of dimensions for the UNet. Use 2 for 2D-UNet and
+                3 for 3D-UNet. Defaults to 2.
+
+        Raises:
+            ValueError: If unsupoorted values are used for padding or ndim.
         """
 
         super().__init__()
-
+        if padding not in ("valid", "VALID", "same", "SAME"):
+            msg = f"Invalid string value for padding: {padding=}. Options are same or valid."
+            raise ValueError(msg)
+        if ndim not in (2, 3):
+            msg = f"Invalid number of dimensions: {ndim=}. Options are 2 or 3."
+            raise ValueError(msg)
         self.depth = depth
         self.in_channels = in_channels
         self.out_channels = out_channels
